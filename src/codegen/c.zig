7706867840033451f4c3fa72656ec3fd1b2554c2
Andrew Kelley <andrew@ziglang.org>
2025-04-16 23:47:05 -0700

    revert most instances of anyerror to match origin/master

--- src/codegen/c.zig
+++ src/codegen/c.zig
@@ -7955,7 +7955,7 @@ const StringLiteral = struct {
         };
     }
 
-    pub fn start(self: *StringLiteral) anyerror!void {
+    pub fn start(self: *StringLiteral) std.io.Writer.Error!void {
         const writer = self.writer;
         if (self.len <= max_string_initializer_len) {
             self.bytes_written += try writer.writeByteCount('\"');
@@ -7964,7 +7964,7 @@ const StringLiteral = struct {
         }
     }
 
-    pub fn end(self: *StringLiteral) anyerror!void {
+    pub fn end(self: *StringLiteral) std.io.Writer.Error!void {
         const writer = self.writer;
         if (self.len <= max_string_initializer_len) {
             self.bytes_written += try writer.writeByteCount('\"');
@@ -7973,7 +7973,7 @@ const StringLiteral = struct {
         }
     }
 
-    fn writeStringLiteralChar(writer: *std.io.BufferedWriter, c: u8) anyerror!usize {
+    fn writeStringLiteralChar(writer: *std.io.BufferedWriter, c: u8) std.io.Writer.Error!usize {
         switch (c) {
             7 => return writer.writeAllCount("\\a"),
             8 => return writer.writeAllCount("\\b"),
@@ -7990,7 +7990,7 @@ const StringLiteral = struct {
         }
     }
 
-    pub fn writeChar(self: *StringLiteral, c: u8) anyerror!void {
+    pub fn writeChar(self: *StringLiteral, c: u8) std.io.Writer.Error!void {
         const writer = self.writer;
         if (self.len <= max_string_initializer_len) {
             if (self.cur_len == 0 and self.bytes_written > 1)
